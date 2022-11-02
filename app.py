a06c13e3293728d585962507686475075a29da5d
Han Jae Chang <abcwockd95@gmail.com>
2022-11-02 14:17:47 +0900

    백엔드 삭제 기능, 꼬릿말

--- app.py
+++ app.py
@@ -20,12 +20,16 @@ def home():
 
 @app.route("/miniproject", methods=["POST"])
 def project_post():
+    comment_list = list(db.comments.find({}, {'_id': False}))
+    count = len(comment_list) + 1
+
     comment_receive = request.form['comment_give']
     name_receive = request.form['name_give']
 
     doc = {
         'comment': comment_receive,
         'names': name_receive,
+        'num': count,
     }
     db.comments.insert_one(doc)
     return jsonify({'msg': '댓글이 작성되었습니다.'})
@@ -35,6 +39,23 @@ def project_post():
 def project_get():
     comment_list = list(db.comments.find({}, {'_id': False}))
     return jsonify({'comments': comment_list})
+
+
+@app.route("/miniproject/delete", methods=["POST"])
+def project_delete():
+    num_receive = request.form['num_give']
+    db.comments.delete_one({'num': int(num_receive)})
+    return jsonify({'msg': '댓글이 삭제되었습니다.'})
+
+
+@app.route("/miniproject/update", methods=["POST"])
+def project_update():
+    comment_receive = request.form['comment_give']
+    num_receive = request.form['num_give']
+    comment_update = db.comments.update_one({'num': int(num_receive)}, {'$set':{'comment': comment_receive}})
+    return jsonify({'comment': comment_update})
+
+
 
 
 if __name__ == '__main__':
--- static/main.js
+++ static/main.js
@@ -1,10 +1,9 @@
+
+
 $(document).ready(function () {
     show_comment()
 });
 
-$("#jqueryBtn").click(function(){
-    $(".blockquote-footer").input();
-});
 // document.getElementById("button").addEventListener("click", save_comment);
 
 function save_comment() {
@@ -33,19 +32,47 @@ function show_comment() {
             for (let i = 0; i < rows.length; i++) {
                 let comment = rows[i]['comment']
                 let names = rows[i]['names']
+                let num = rows[i]['num']
 
-                let temp_html = `
-                <div class="card">
+                let temp_html =
+            ` <div class="card">
                 <div class="card-body">
                     <blockquote class="blockquote mb-0">
                         <p class="text1">${names}</p>
                         <footer class="blockquote-footer">${comment}</footer>
+                        <button onclick="delete_comment(${num})" type="button">삭제</button>
+                        <button onclick="update_comment(${num})" type="button">업데이트</button>
                     </blockquote>
                 </div>
-            </div>
-           `
+            </div>`
                 $("#comment-list").append(temp_html);
             }
         }
     });
 }
+
+function update_comment(num) {
+    let comment = $("#comment").val()
+    $.ajax({
+
+                type: "POST",
+                url: "/miniproject/update",
+                data: {'comment_give': comment, 'num_give': num },
+                success: function (response) {
+                    alert(response["msg"])
+                      window.location.reload()
+                }
+            });
+}
+
+function delete_comment(num) {
+            $.ajax({
+                type: "POST",
+                url: "/miniproject/delete",
+                data: {'num_give': num},
+                success: function (response) {
+                    alert(response["msg"])
+                      window.location.reload()
+                }
+            });
+        }
