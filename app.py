f7cf09c5dec7efa137459da150d3188f81e152a6
Han Jae Chang <abcwockd95@gmail.com>
2022-11-03 19:42:08 +0900

    완성1

--- app.py
+++ app.py
@@ -16,10 +16,41 @@ db = client.dbsparta
 @app.route('/')
 def home():
     return render_template('index.html')
-    
+
+
 @app.route('/youngjae')
 def youngjae():
     return render_template('youngjae.html')
+
+
+@app.route('/sogae')
+def sogae():
+    return render_template('sogae.html')
+
+
+@app.route('/heera')
+def heera():
+    return render_template('heera.html')
+
+
+@app.route('/jaechang')
+def jaechang():
+    return render_template('jaechang.html')
+
+
+@app.route('/sogaehee')
+def sogaehee():
+    return render_template('sogaehee.html')
+
+
+@app.route('/advance')
+def sogaehee():
+    return render_template('advance.html')
+
+
+@app.route('/style')
+def sogaehee():
+    return render_template('style.html')
 
 
 @app.route("/miniproject", methods=["POST"])
@@ -50,7 +81,6 @@ def project_delete():
     num_receive = request.form['num_give']
     db.comments.delete_one({'num': int(num_receive)})
     return jsonify({'msg': '댓글이 삭제되었습니다.'})
-
 
 
 if __name__ == '__main__':
